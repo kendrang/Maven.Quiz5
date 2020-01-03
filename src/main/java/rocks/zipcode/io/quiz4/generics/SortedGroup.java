8574c47d5db16986bd5e89ce37323c0be0da4528
kendrang <kendrang@users.noreply.github.com>
2020-01-03 11:35:04 -0500

    half an hour left

--- src/main/java/rocks/zipcode/io/quiz4/generics/SortedGroup.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/SortedGroup.java
@@ -1,18 +1,32 @@
 package rocks.zipcode.io.quiz4.generics;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class SortedGroup<_> extends Group<_> {
+
+    ArrayList<_> sorted = new ArrayList<>();
+    public SortedGroup() {
+
+    }
+
     @Override
     public void insert(_ value) {
+        sorted.add(value);
+
     }
 
     @Override
     public void delete(_ value) {
+        sorted.remove(value);
     }
 
     public Integer indexOf(_ value) {
-        return null;
+        return sorted.indexOf(value);
     }
 }
